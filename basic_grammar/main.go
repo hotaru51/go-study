282cc5d36f39f3a9624a837a814fb2323622dc13
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-04-11 23:06:45 +0900

    fmt: print系

---
+++ basic_grammar/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    // fmt.Println
+    // 複数渡すとスペース区切りで出力
+    fmt.Println("auie")
+    fmt.Println("nocchi", "kashiyuka", "a-chan")
+
+    // fmt.Printf
+    fmt.Printf("a = %d\n", 5)
+}
