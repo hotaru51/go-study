5c15e3f404f95027b0ee51f89cc0456513c88345
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-04-12 22:20:36 +0900

    fmt: %v, %#v, %t, print, println

--- basic_grammar/main.go
+++ basic_grammar/main.go
@@ -12,4 +12,16 @@ func main() {
 
     // fmt.Printf
     fmt.Printf("a = %d\n", 5)
+
+    // %v, %#v, %T
+    fmt.Printf("数値=%v, 文字列=%v, 配列=%v\n", 417, "渡辺曜", [...]int{4, 1, 7})
+    fmt.Printf("数値=%#v, 文字列=%#v, 配列=%#v\n", 417, "渡辺曜", [...]int{4, 1, 7})
+    fmt.Printf("数値=%T, 文字列=%T, 配列=%T\n", 417, "渡辺曜", [...]int{4, 1, 7})
+
+    // Print, Println
+    print("hanamaru ")
+    print("ruby ")
+    print("yoshiko ")
+    println("yohane")
+    println("dia kanan mari")
 }
