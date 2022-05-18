a7c9ff2167bc4323a33fc5f144e269ec7cef63db
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-05-18 23:27:33 +0900

    labelとcontinue

--- basic_grammar/main.go
+++ basic_grammar/main.go
@@ -330,6 +330,17 @@ func main() {
 				}
 			}
 		}
+
+	L:
+		for i := 1; i <= 3; i++ {
+			for j := 1; j<= 3; j++ {
+				if j > 1 {
+					continue L
+				}
+				fmt.Printf("%d * %d = %d\n", i, j, i *j)
+			}
+			fmt.Println("yohane")
+		}
 }
 
 // int xとyを足して返す
