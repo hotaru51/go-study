defc94731afc63853fcc1f0a0de5ad7e2dc17573
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-06-06 21:25:07 +0900

    チャンネルを介してgoroutineに値を送信

---
+++ go_channel/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// チャンネルの定義
+	var ch chan int
+	// 受信専用チャンネル
+	// var receiveOnly <-chan int
+	// 送信専用チャンネル
+	//var sendOnly chan<- int
+
+	// makeでの生成
+	ch = make(chan int)
+	// サイズ指定も可能
+	ch = make(chan int, 8)
+	// 送信用、受信用
+	// receiveOnly = make(<-chan int, 5)
+	// sendOnly = make(chan<- int, 5)
+
+	// チャンネルへの値送信
+	ch <- 5
+	// チャンネルから値取得
+	i := <-ch
+	fmt.Printf("i = %d\n", i)
+
+	// この状態で下記のコメントアウトを外すと永久に受信待ちでランタイムエラーとなる
+	// fmt.Println(<-ch)
+
+	// goroutineで受信
+	go receiver(ch)
+
+	// バッファのサイズはcapで参照できる
+	fmt.Printf("cap(ch) = %d\n", cap(ch))
+	for i := 0; i < 10; i++ {
+		ch <- i
+		// バッファ内に格納されている要素数はlenで取れる
+		fmt.Printf("len(ch) = %d\n", len(ch))
+	}
+}
+
+func receiver(ch <-chan int) {
+	for {
+		i := <-ch
+		fmt.Println(i)
+	}
+}
