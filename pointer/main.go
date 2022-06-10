bf832247621d5a03b07e99b9e9d4c07fd91c9058
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-06-10 22:03:39 +0900

    関数への参照渡し

--- pointer/main.go
+++ pointer/main.go
@@ -13,4 +13,18 @@ func main() {
 	i := 5
 	pi := &i
 	fmt.Printf("pi => %T\n", pi)
+
+	// 関数への参照渡し
+	i2 := 1
+	fmt.Printf("i2 = %d\n", i2)
+	inc(&i2)
+	fmt.Printf("i2 = %d\n", i2)
+	inc(&i2)
+	fmt.Printf("i2 = %d\n", i2)
+	inc(&i2)
+	fmt.Printf("i2 = %d\n", i2)
+}
+
+func inc(i *int) {
+	*i++
 }
