99481bc83779ef59ea4c53c710ee226fb05b4754
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-06-10 22:00:07 +0900

    変数のアドレス取得

--- pointer/main.go
+++ pointer/main.go
@@ -7,5 +7,10 @@ import (
 func main() {
 	// ポインタ型の初期値はnil
 	var p *int
-	fmt.Printf("(p == nil) = %v", (p == nil))
+	fmt.Printf("(p == nil) = %v\n", (p == nil))
+
+	// 変数のアドレス取得
+	i := 5
+	pi := &i
+	fmt.Printf("pi => %T\n", pi)
 }
