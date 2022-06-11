1ccadd5c7f717dce1805ea733867f344db95b4b3
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-06-11 11:11:39 +0900

    配列ポインタの参照

--- pointer/main.go
+++ pointer/main.go
@@ -23,8 +23,40 @@ func main() {
 	fmt.Printf("i2 = %d\n", i2)
 	inc(&i2)
 	fmt.Printf("i2 = %d\n", i2)
+
+	// 配列の参照
+	intArr := [3]int{1, 2, 3}
+	fmt.Printf("intArr = %v\n", intArr)
+	pow(&intArr)
+	fmt.Printf("intArr = %v\n", intArr)
+
+	// 配列参照時の記載の省略
+	strArr := [3]string{"you", "chika", "ruby"}
+	pstrArr := &strArr
+	fmt.Println(strArr[0])
+	// (*pstrArr)[0] という記載でなくても自動で参照してくれる
+	fmt.Println(pstrArr[0])
+	fmt.Println(pstrArr[1])
+	fmt.Println(pstrArr[2])
+
+	// rangeでも省略可能
+	for _, str := range pstrArr {
+		fmt.Println(str)
+	}
+	// len、cap等も省略可能
+	fmt.Printf("len(pstrArr) = %v, cap(pstrArr) = %v, pstrArr[0:2] = %v\n",
+		len(pstrArr), cap(pstrArr), pstrArr[0:2],
+	)
 }
 
 func inc(i *int) {
 	*i++
+}
+
+func pow(p *[3]int) {
+	i := 0
+	for i < 3 {
+		(*p)[i] = (*p)[i] * (*p)[i]
+		i++
+	}
 }
