3670ed9df27ce01bcf3339524d7a573e71bb715f
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-06-29 15:59:11 +0900

    ファイルのopen/close

--- os/main.go
+++ os/main.go
@@ -6,24 +6,35 @@ import (
 	// "log"
 )
 
+func openFile(path string) {
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// deferで開いたファイルを確実にcloseする
+	fmt.Println(f.Name())
+	defer f.Close()
+}
+
 func main() {
 	// os.Exitで終了するとdeferは実行されない
 	defer func() {
 		fmt.Println("defer")
 	}()
 
-	/*
 	// 存在しないファイルを開こうとした場合にエラーメッセージを出力
 	_, err := os.Open("hoge")
 	if err != nil {
-		log.Fatal(err)
+		// log.Fatal(err)
 	}
-	*/
 
 	// コマンドライン引数はos.Args[]で参照できる
 	for i, v := range os.Args {
 		fmt.Printf("os.Args[%d] = \"%s\"\n", i, v)
 	}
+
+	openFile("./uranohoshi.txt")
 
 	os.Exit(0)
 }
