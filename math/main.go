3da3adc6a88035ebf11924a3b9eca22d05c390b3
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-08-12 23:58:52 +0900

    mathパッケージの定数

---
+++ math/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// 様々な定数を含む
+	fmt.Printf("Pi = %v\n", math.Pi)
+	fmt.Printf("Sqrt2 = %v\n", math.Sqrt2)
+
+	// 数値型に関する定数の一部
+	fmt.Printf("MaxInt32 = %v\n", math.MaxInt32)
+	fmt.Printf("MaxFloat32 = %v\n", math.MaxFloat32)
+	fmt.Printf("MaxInt64 = %v\n", math.MaxInt64)
+	fmt.Printf("MaxFloat64 = %v\n", math.MaxFloat64)
+}
