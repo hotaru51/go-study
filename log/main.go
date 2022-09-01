45597707d1eac174b807e820d39413b0b07f5373
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-09-01 20:19:50 +0900

    ログの出力先を変更する

--- log/main.go
+++ log/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"log"
+	"os"
 )
 
 func main() {
 	log.Print("aaaaa\n")
 	log.Println("bbbbb")
 	log.Printf("%s\n", "ccccc")
+
+	// デフォルトでは標準エラー出力になるので、標準出力に変更する
+	log.SetOutput(os.Stdout)
+	log.Println("stdout")
 }
