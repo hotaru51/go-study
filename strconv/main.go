5ecd3d41e40bdee829b4d96643ab6c0db15d7510
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-09-05 21:03:11 +0900

    strconv: 浮動小数点型をstring型に変換

--- strconv/main.go
+++ strconv/main.go
@@ -16,4 +16,11 @@ func main() {
 	// 第2引数は基数
 	stri := strconv.FormatInt(i, 10)
 	fmt.Printf("stri = %s\n", stri)
+
+	// float -> string
+	f := float64(445.745)
+	strf1 := strconv.FormatFloat(f, 'f', 2, 64)
+	fmt.Printf("strf1 = %s\n", strf1)
+	strf2 := strconv.FormatFloat(f, 'g', -1, 64)
+	fmt.Printf("strf2 = %s\n", strf2)
 }
