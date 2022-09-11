599fbbec59fbe41e9ce1f4a9f94b79cd015ccf41
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-09-11 22:54:34 +0900

    文字列の検索

--- strings/main.go
+++ strings/main.go
@@ -9,4 +9,17 @@ func main() {
 	// []string内の要素を結合する
 	cyaron := []string{"you", "chika", "ruby"}
 	fmt.Println(strings.Join(cyaron, "! ") + "!")
+
+	// 文字列の検索
+	fmt.Printf("Index(\"ABCDE\", \"A\") = %d\n", strings.Index("ABCDE", "A"))
+	fmt.Printf("Index(\"ABCDE\", \"BCD\") = %d\n", strings.Index("ABCDE", "BCD"))
+	fmt.Printf("Index(\"ABCDE\", \"Y\") = %d\n", strings.Index("ABCDE", "Y"))
+
+	// 最後に文字列が見つかる部分を検索
+	fmt.Printf("LastIndex(\"ABCDEABCD\", \"B\") = %d\n", strings.LastIndex("ABCDEABCD", "B"))
+
+	// 指定した文字列のいずれかの文字が最初に見つかる部分を返す
+	fmt.Printf("IndexAny(\"ABCDEABCD\", \"CDE\") = %d\n", strings.IndexAny("ABCDEABCD", "CDE"))
+	//IndexAnyの最後に見つかる方版
+	fmt.Printf("LastIndexAny(\"ABCDEABCD\", \"CDE\") = %d\n", strings.LastIndexAny("ABCDEABCD", "CDE"))
 }
