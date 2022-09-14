ce3cb257aaa4dc406e86f42ebf4e0f99485d2a14
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-09-14 22:59:44 +0900

    指定した文字列・文字が含まれているか検証

--- strings/main.go
+++ strings/main.go
@@ -28,4 +28,15 @@ func main() {
 	fmt.Printf("HasPrefix(\"ことほのうみ\", \"ほの\") = %t\n", strings.HasPrefix("ことほのうみ", "ほの"))
 	fmt.Printf("HasSuffix(\"ことほのうみ\", \"うみ\") = %t\n", strings.HasSuffix("ことほのうみ", "うみ"))
 	fmt.Printf("HasSuffix(\"ことほのうみ\", \"ほの\") = %t\n", strings.HasSuffix("ことほのうみ", "ほの"))
+
+	// 指定した文字列が含まれるか検証
+	fmt.Printf("Contains(\"ABCDE\", \"AB\") = %t\n", strings.Contains("ABCDE", "AB"))
+	fmt.Printf("Contains(\"ABCDE\", \"XY\") = %t\n", strings.Contains("ABCDE", "XY"))
+	fmt.Printf("Contains(\"ABCDE\", \"\") = %t\n", strings.Contains("ABCDE", ""))
+	fmt.Printf("Contains(\"\", \"\") = %t\n", strings.Contains("", ""))
+
+	// 指定した文字列のいずれかの文字が含まれているか検証
+	fmt.Printf("ContainsAny(\"XYZ\", \"XY\") = %t\n", strings.ContainsAny("XYZ", "XY"))
+	fmt.Printf("ContainsAny(\"XYZ\", \"YOSHIKO\") = %t\n", strings.ContainsAny("XYZ", "YOSHIKO"))
+	fmt.Printf("ContainsAny(\"XYZ\", \"HANAMARU\") = %t\n", strings.ContainsAny("XYZ", "HANAMARU"))
 }
